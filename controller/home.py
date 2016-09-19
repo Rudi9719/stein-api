24497c3585ed1f725f1ebf7c9e9e69f1077c3a57
Gregory Rudolph <rudi@lolay.com>
2016-09-19 15:54:45 -0400

    Removed Blog.

--- controller/home.py
+++ controller/home.py
@@ -2,9 +2,7 @@
 
 from controller.base import BaseController
 from frameworks.bottle import view, static_file
-from blog.blogger import Blogger
 
-blog_mgr = Blogger()
 
 class HomeController(BaseController):
     
@@ -12,11 +10,4 @@ class HomeController(BaseController):
     @view('home/index')
     def index(self):
         pass
-    #return dict(title='Home', page_name='Home', latest_post=blog_mgr.last_post())
-
-#@view('home/index')
-#def blog(self):
-#return dict(title='Blog', page_name='Blog', latest_post=blog_mgr.all_posts())
-
-
 
